e5d2e10d5d4d5773a915dd5fa3721c23ecabfc08
Felipe Alvares de Azevedo Macedo <felipemacedo@corporigido.xyz>
2021-05-01 09:10:33 +0200

    teste com os scrapers

--- src/data.js
+++ src/data.js
@@ -2,3 +2,5 @@ const websites = {
     botafogo:   ["https://soubotafogo.bfr.com.br/#/publico/home", '.sb_22'],
     santos:     ["https://sociorei.com/", '.app-header > div:nth-child(0)'],
 }
+
+console.log(websites)
--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 const puppeteer = require('puppeteer');
 
-
 (async ()=>{
-    const browser = await puppeteer.launch({ 
-        headless: false,
-        slowMo: 250,
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage()
-    await page.goto("https://sociorei.com/", {waitUntil: 'domcontentloaded'})
 
-    const data = await page.evaluateHandle(()=>{
-        // const numSocios = document.querySelector('body > main > section > div.container.sb_25.ng-scope > h2 > span.sb_23.ng-binding').innerHTML
-        return{ 
-            socioRei: document.querySelector(".app-header > div:nth-child(0)").innerHTML
+
+    await page.goto("https://soubotafogo.bfr.com.br/#/publico/home", {
+        waitUntil: 'domcontentloaded',
+    })
+
+    const data = await page.evaluate(()=>{
+
+        let numSocios = document.querySelector(".sb_23").innerHTML;
+
+        return {
+            numSocios
         }
     })
+
+    console.log(data)
+
 
     await browser.close()
 })
