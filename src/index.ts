8ecb0e35991304fe0cb5aa53967e92285e496c98
Felipe Alvares de Azevedo Macedo <felipemacedo@corporigido.xyz>
2021-05-02 12:08:28 +0200

    ADD: worked data to get correct result

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer'
+import fs from 'fs'
 
 (async () => {
     const data: {teamName:string, website:string, cssSelector:string[]}[] = [
@@ -32,7 +33,6 @@ import puppeteer from 'puppeteer'
      */
     const browser = await puppeteer.launch({
         headless: false,
-        slowMo: 50,
     });
     const page = await browser.newPage()
     const timeout = 5000
@@ -47,35 +47,53 @@ import puppeteer from 'puppeteer'
 
         //LOAD PAGE
         // wait for the selector to be shown in the page, then more 5sec for the javascript code to run so that we have the number of team members.
-        console.log(`Waiting for ${data[i].website} CSS Selector to load`)
+        console.log(`WORK: Waiting for ${data[i].website} CSS Selector to load`)
         await page.waitForSelector(data[i].cssSelector[0])
 
 
-        console.log(`Waiting ${timeout} ms for the Javascript page code to run`)
+        console.log(`WORK: Waiting ${timeout} ms for the Javascript page code to run`)
         await page.waitForTimeout(timeout)
 
         /**
          * Evaluating page by using the inspect and getting the data
          */
 
-        console.log(`Inspecting page to scrap the number of team members`)
+        console.log(`WORK: Inspecting page to scrap the number of team members`)
         let scrap:string[]=[]
         for(let j=0; j< data[i].cssSelector.length; j++){
             scrap.push(
                 await page.evaluate((selector) => {
-                var numSocios = document.querySelector(selector)!.innerHTML;
-                return numSocios
-                }, data[i].cssSelector[0])
+                    var numSocios = document.querySelector(selector)!.innerHTML;
+                    return numSocios
+                }, data[i].cssSelector[j])
             )
         }
 
         const teamName:string = data[i].teamName
-        const nuMembers:string = scrap[0]
-        // const nuMembers:string = scrap.reduce(function (total, num){
-            // return total + num
-        // })
 
-        console.log(`pushing team name and number of team members into output data`)
+        // MANAGE SCRAP INTO A STRING NUMBER
+        
+        console.log(`WORK: Reducing ${data[i].teamName} scraped data into a single string`)
+        const scrapedMembers:string = scrap.reduce(function (total, num){
+            return total + num
+        })
+
+        console.log(`WORK: Spliting ${data[i].teamName} reduced data to take out "dot" separators, and reducing the result`)
+        const nuMembersWithoutDotSeparation:string[] = scrapedMembers.split('.')
+        const nuMembersWithSpaceSeparation:string = nuMembersWithoutDotSeparation.reduce(function(total, num){
+            return total + num
+        })
+
+        console.log(`WORK: Spliting again ${data[i].teamName} reduced data to take out "space" separators, and applying final reduce of data`)
+        const nuMembersWithOutSpaceSeparation:string[] = nuMembersWithSpaceSeparation.split(' ')
+        const nuMembers:string = nuMembersWithOutSpaceSeparation.reduce(function(total, num){
+            return total + num
+        })
+
+        //FINISH SCRAP DATA INTO STRING NUMBER
+
+
+        console.log(`WORK: Pushing team name and number of team members into output data`)
         scrapData.push({ teamName, nuMembers })
         console.log(scrapData)
     }
@@ -83,6 +101,9 @@ import puppeteer from 'puppeteer'
     console.log('DATA SCRAPED') 
     console.log('---------------------------') 
     console.log(scrapData)
+
+    console.log('Creating file at "./output/scrapResult.txt')
+    await fs.writeFileSync('./outputs/scrapResult.txt', scrapData.toString())
 
     // Closing browser
     await browser.close()
