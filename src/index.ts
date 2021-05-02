683d647fae6cb98ddcb06099c3faf159ccd2dc87
Felipe Alvares de Azevedo Macedo <felipemacedo@corporigido.xyz>
2021-05-02 10:32:31 +0200

    FIX: Web scraping workin in pages need push select

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import puppeteer from 'puppeteer'
 
 (async () => {
-    const data: {website:string, cssSelector:string[]}[] = [
-        { website: "https://soubotafogo.bfr.com.br/#/publico/home", cssSelector: [".sb_23"] },
-        { website: "https://sociorei.com/", cssSelector: ["body > app-root > div > app-header > header > div > div > app-counter > div > div > div.counter"] }
+    const data: {teamName:string, website:string, cssSelector:string[]}[] = [
+        { 
+            teamName: "Botafogo", 
+            website: "https://soubotafogo.bfr.com.br/#/publico/home", 
+            cssSelector: [".sb_23"] 
+        },
+        { 
+            teamName: "Santos", 
+            website: "https://sociorei.com/", 
+            cssSelector: ["body > app-root > div > app-header > header > div > div > app-counter > div > div > div.counter"] 
+        },
+        {   
+            teamName: "Vasco", 
+            website: "https://sociogigante.com/", 
+            cssSelector: [
+                ".header-row-1-right > app-scoreboard:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)", 
+                ".header-row-1-right > app-scoreboard:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1)", 
+                ".header-row-1-right > app-scoreboard:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1)", 
+                ".header-row-1-right > app-scoreboard:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1)", 
+                ".header-row-1-right > app-scoreboard:nth-child(1) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1)",
+                ".header-row-1-right > app-scoreboard:nth-child(1) > div:nth-child(1) > div:nth-child(6) > div:nth-child(1)"
+            ]
+        }
     ]
-    const scrapData:string[] = []
+    const scrapData:{teamName:string, nuMembers:string}[] = []
 
     /**
      * Opening browser and a new tab
@@ -15,8 +35,9 @@ import puppeteer from 'puppeteer'
         slowMo: 50,
     });
     const page = await browser.newPage()
+    const timeout = 5000
 
-    for (let i:number = 0; data.length - 1; i++){
+    for (let i:number = 0; i < data.length; i++){
         /**
         * Going to the page we want to scrap   
         */ 
@@ -24,28 +45,44 @@ import puppeteer from 'puppeteer'
             waitUntil:'domcontentloaded'
         })
 
-        console.log(data[i].cssSelector[0])
         //LOAD PAGE
-        // wait page fully loaded, in case of a SPA
-        await page.waitForNavigation({
-            waitUntil:'load'
-        })
+        // wait for the selector to be shown in the page, then more 5sec for the javascript code to run so that we have the number of team members.
+        console.log(`Waiting for ${data[i].website} CSS Selector to load`)
+        await page.waitForSelector(data[i].cssSelector[0])
 
-        // await page.waitForSelector(data[i].cssSelector[0])
+
+        console.log(`Waiting ${timeout} ms for the Javascript page code to run`)
+        await page.waitForTimeout(timeout)
 
         /**
          * Evaluating page by using the inspect and getting the data
          */
-        console.log(data[i].cssSelector[0])
-        let scrap = await page.evaluate((selector) => {
-            var numSocios = document.querySelector(selector)!.innerHTML;
-            return numSocios
-        }, data[i].cssSelector[0])
 
-        scrapData.push(scrap)
+        console.log(`Inspecting page to scrap the number of team members`)
+        let scrap:string[]=[]
+        for(let j=0; j< data[i].cssSelector.length; j++){
+            scrap.push(
+                await page.evaluate((selector) => {
+                var numSocios = document.querySelector(selector)!.innerHTML;
+                return numSocios
+                }, data[i].cssSelector[0])
+            )
+        }
+
+        const teamName:string = data[i].teamName
+        const nuMembers:string = scrap[0]
+        // const nuMembers:string = scrap.reduce(function (total, num){
+            // return total + num
+        // })
+
+        console.log(`pushing team name and number of team members into output data`)
+        scrapData.push({ teamName, nuMembers })
         console.log(scrapData)
     }
-
+    console.log('---------------------------') 
+    console.log('DATA SCRAPED') 
+    console.log('---------------------------') 
+    console.log(scrapData)
 
     // Closing browser
     await browser.close()
